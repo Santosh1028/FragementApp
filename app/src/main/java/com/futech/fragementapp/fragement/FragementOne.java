5546ef632629c8df8b25a0e6e4948f93f511c1d1
santosh102 <santoshmadani102@gmail.com>
2021-12-06 18:12:48 -0800

    initial commit

--- app/src/main/java/com/futech/fragementapp/fragement/FragementOne.java
+++ app/src/main/java/com/futech/fragementapp/fragement/FragementOne.java
@@ -1,5 +1,6 @@
 package com.futech.fragementapp.fragement;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.futech.fragementapp.R;
+import com.futech.fragementapp.SecondActivity;
 
 public class FragementOne extends Fragment {
+
+    TextView tv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +23,15 @@ public class FragementOne extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragement_one, container, false);
 
+        tv=view.findViewById(R.id.tvFirst);
 
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         return view;
